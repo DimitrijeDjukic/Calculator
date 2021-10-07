038ae4829efb7b5e108b109fc57d8b4b8132685c
Dimitrije Djukic <dimitrijedjkc@gmail.com>
2021-10-07 22:59:26 +0200

    js changes

--- calc.js
+++ calc.js
@@ -1,15 +1,19 @@
 const lis = document.querySelectorAll("ul li")
 
-lis.forEach((node) =>{
+lis.forEach((node) => {
     node.addEventListener('mousedown', function(event) {
         const value = node.innerText
         const result = document.querySelector(".result")
         const resultText = result.innerText;
 
+        if(resultText == '0' || resultText == 'undefined' || resultText == 'Infinity') {
+            result.innerText = '';
+        }
 
         if(value == '=') {
             let solution = eval(resultText)
             result.innerText = solution;
+            return true;
         }
 
 
@@ -19,8 +23,17 @@ lis.forEach((node) =>{
             return true
         }
 
+        if(value == 'Binar to Dezimal') {
+            result.innerText = parseInt(resultText, 2)
+            return true
+        }
+
+      
+
       
         result.append(value);
+
+        
 
 
  
